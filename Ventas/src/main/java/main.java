44f17f3b8d804cc9118ba38e34d7ffe178d5583b
jenny <jcarolina.escobar@udea.edu.co>
2021-05-16 01:34:07 -0500

    introduce method in venta class

--- Ventas/src/main/java/main.java
+++ Ventas/src/main/java/main.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
         int perroPrice = 6000, hamburguesaPrice = 10000, perroCuantityToBuy = 0, hamburguesaCuantityToBuy = 0,
-                productSelecction = 0, cuantityProductSelection, endSale = 0, totalPrice, totalPricePerrosBought = 0, totalPriceHamburgesasBought = 0;
+                productSelecction = 0, cuantityProductSelection, endSale = 0, totalPrice, totalPricePerrosBought = 0,
+                totalPriceHamburgesasBought = 0;
 
         boolean takeOrder = true;
         Scanner takeOrders = new Scanner(System.in);
@@ -11,22 +12,16 @@ public class main {
         System.out.println("Venta de perros y hamburguesas");
 
         while (takeOrder) {
-
-            System.out.println("Seleccione producto: ");
-            System.out.println("1. Perro \n2. Hamburguesa");
-            productSelecction = Integer.parseInt(takeOrders.nextLine());
-            System.out.println("Indique cantidad: ");
-            cuantityProductSelection = Integer.parseInt(takeOrders.nextLine());
+            productSelecction = productToSell(takeOrders);
+            cuantityProductSelection = cuantityToSell(takeOrders);
             if (productSelecction == 1) {
-                perroCuantityToBuy = perroCuantityToBuy + cuantityProductSelection;
-            } else if (productSelecction == 2) {
-                hamburguesaCuantityToBuy = hamburguesaCuantityToBuy + cuantityProductSelection;
+                perroCuantityToBuy +=  cuantityProductSelection;
+            } else if (productSelecction== 2) {
+                hamburguesaCuantityToBuy +=  cuantityProductSelection;
             } else {
                 break;
             }
-            System.out.println("Desea finalizar o agregar otro producto ?");
-            System.out.println("1. Agregar \n2. Finalizar");
-            endSale = Integer.parseInt(takeOrders.nextLine());
+            endSale = takeMoreOrders(takeOrders);
             if (endSale == 1) {
                 takeOrder = true;
             } else if (endSale == 2) {
@@ -36,9 +31,33 @@ public class main {
             }
         }
 
-        totalPriceHamburgesasBought = hamburguesaPrice * hamburguesaCuantityToBuy;
-        totalPricePerrosBought = perroPrice * perroCuantityToBuy;
-        totalPrice = totalPriceHamburgesasBought + totalPricePerrosBought;
+        totalPriceHamburgesasBought = sellProduct(hamburguesaPrice,hamburguesaCuantityToBuy);
+        totalPricePerrosBought = sellProduct(perroPrice,perroCuantityToBuy);
+        getTotalPrice(totalPriceHamburgesasBought,totalPricePerrosBought);
+    }
+
+    private static void getTotalPrice(int totalPriceHamburgesasBought, int totalPricePerrosBought) {
+        int totalPrice = totalPriceHamburgesasBought + totalPricePerrosBought;
         System.out.println("Total: " + totalPrice);
     }
+
+    public static int takeMoreOrders(Scanner takeOrders){
+        System.out.println("Desea finalizar o agregar otro producto ?");
+        System.out.println("1. Agregar \n2. Finalizar");
+        return Integer.parseInt(takeOrders.nextLine());
+    }
+    private static int productToSell(Scanner takeOrders){
+        System.out.println("Seleccione producto: ");
+        System.out.println("1. Perro \n2. Hamburguesa");
+        return Integer.parseInt(takeOrders.nextLine());
+    }
+    private static int cuantityToSell(Scanner takeOrders){
+        System.out.println("Indique cantidad: ");
+        return Integer.parseInt(takeOrders.nextLine());
+    }
+
+    private static int sellProduct(int price, int cuantity){
+        return price*cuantity;
+    }
+
 }
